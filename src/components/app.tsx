f7331fa34032e600ad3194878878bb23f66ae23a
Marcin Tatoń <tatonmarcinpr@gmail.com>
2022-09-24 02:57:41 +0200

    Added netlify config file

--- src/components/app.tsx
+++ src/components/app.tsx
@@ -1,10 +1,32 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import api from '@src/services/apiService';
 
 export default function App() {
+  const [title, setTitle] = useState('empty');
+
+  useEffect(() => {
+    const getData = async () => {
+      const response = await api.get(
+        'https://songbook-backend-production.up.railway.app/other'
+      );
+      return response;
+    };
+
+    getData()
+      .then((data) => {
+        if (data) setTitle(data[0].title);
+      })
+      .catch((e: Error) => {
+        console.log(e.message);
+        setTitle('error');
+      });
+  }, []);
+
   return (
     <div>
       <React.StrictMode>
-        <>Hello world</>
+        <>Hello world </>
+        {title}
       </React.StrictMode>
     </div>
   );
