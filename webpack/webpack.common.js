3223a83701ac733d6db960bb30e567673aad414a
Marcin Tatoń <tatonmarcinpr@gmail.com>
2022-09-25 13:19:29 +0200

    Webpack update

--- webpack/webpack.common.js
+++ webpack/webpack.common.js
@@ -52,9 +52,9 @@ module.exports = {
     filename: '[name].bundle.js',
   },
   optimization: {
-    // splitChunks: {
-    //   chunks: 'all',
-    // },
+    splitChunks: {
+      chunks: 'all',
+    },
   },
   plugins: [
     new HtmlWebpackPlugin({
--- webpack/webpack.prod.js
+++ webpack/webpack.prod.js
@@ -23,5 +23,6 @@ module.exports = {
   },
   output: {
     clean: true,
+    publicPath: '/',
   },
 };
