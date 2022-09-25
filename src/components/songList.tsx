2f5e2a4a458373ef2b9f1096964e93ba3fcd584d
Marcin Tatoń <tatonmarcinpr@gmail.com>
2022-09-26 01:25:19 +0200

    finished main view. Refactoring due tomorrow

--- src/components/songList.tsx
+++ src/components/songList.tsx
@@ -9,10 +9,14 @@ import {
   InputRightElement,
   VStack,
   Text,
-  Badge,
   Tag,
+  SimpleGrid,
   GridItem,
-  Grid,
+  Accordion,
+  AccordionButton,
+  AccordionIcon,
+  AccordionItem,
+  AccordionPanel,
 } from '@chakra-ui/react';
 import { SearchIcon } from '@chakra-ui/icons';
 import { Link } from 'react-router-dom';
@@ -25,7 +29,7 @@ export default function SongList() {
   const [metadata, setMetadata] = useState<ISongMetadata[]>([]);
   const [tags, setTags] = useState<ITag[]>([]);
 
-  // const [selectedTags, setSelectedTags] = useState<string[]>([]);
+  const [selectedTags, setSelectedTags] = useState<ITag[]>([]);
   const [searchTerm, setSearchTerm] = useState<string>('');
 
   useEffect(() => {
@@ -45,7 +49,13 @@ export default function SongList() {
   }, []);
 
   const songs = metadata;
-  const searchFilteredSongs = songs.filter((song) =>
+  const tagFilteredSongs = songs.filter((song) =>
+    song.tags.some((tag) => selectedTags.map((x) => x.name).includes(tag))
+  );
+
+  const areTagFiltersActive = tags.length !== selectedTags.length;
+
+  const searchFilteredSongs = tagFilteredSongs.filter((song) =>
     song.title.toLowerCase().includes(searchTerm.toLowerCase())
   );
 
@@ -64,65 +74,151 @@ export default function SongList() {
 
     getData()
       .then((data) => {
-        if (data) setTags(data);
+        if (data) {
+          setTags(data);
+          setSelectedTags(data);
+        }
       })
       .catch((e: Error) => {
         console.log(e.message);
         setTags([]);
+        setSelectedTags([]);
       });
   }, []);
 
-  console.log(searchTerm);
-
   return (
     <>
-      <Center>
-        <VStack p={'2em'}>
-          <Heading as="h1" size="4xl" textAlign={'center'}>
-            Śpiewnik Oazowy
-          </Heading>
-          <Heading as="h6" size="lg" textAlign={'center'}>
-            Oaza Dorosłych Knurów
-          </Heading>
-          <InputGroup pt={'5em'}>
-            <Input
-              value={searchTerm}
-              onChange={(event) => setSearchTerm(event.target.value)}
-              placeholder="Szukaj piosenek..."
-              size="lg"
-            />
-            <InputRightElement pt={'5em'} pr={'0.5em'}>
-              {searchTerm !== '' && (
-                <Tag mt={'3.3em'}>{searchFilteredSongs.length}</Tag>
-              )}
-              {searchTerm === '' && <SearchIcon mt={'3em'} color={'grey'} />}
-            </InputRightElement>
-          </InputGroup>
-          <Divider orientation="horizontal" pt={'2em'} />
+      <SimpleGrid
+        columns={[1, 1, 3]}
+        templateColumns={['1fr', '1fr', 'repeat(5, 1fr)']}
+      >
+        <GridItem colSpan={[0, 0, 1]}></GridItem>
+        <GridItem colSpan={[0, 0, 3]}>
+          <Center margin={'auto'} maxWidth={'41.8em'}>
+            <VStack p={'2em'}>
+              <Heading
+                pt={'0.4em'}
+                as="h1"
+                size="4xl"
+                textAlign={'center'}
+                bgGradient={'linear(to-r, blue.500, purple.500)'}
+                bgClip={'text'}
+              >
+                Śpiewnik Oazowy
+              </Heading>
+              <Heading as="h6" size="lg" textAlign={'center'}>
+                Oaza Dorosłych Knurów
+              </Heading>
+              <InputGroup pt={'5em'}>
+                <Input
+                  value={searchTerm}
+                  onChange={(event) => setSearchTerm(event.target.value)}
+                  placeholder="Szukaj piosenek..."
+                  size="lg"
+                />
+                <InputRightElement pt={'5em'} pr={'0.5em'}>
+                  {searchTerm !== '' && (
+                    <Tag color={'grey'} mt={'3.3em'}>
+                      {searchFilteredSongs.length}
+                    </Tag>
+                  )}
+                  {searchTerm === '' && (
+                    <SearchIcon mt={'3em'} color={'grey'} />
+                  )}
+                </InputRightElement>
+              </InputGroup>
 
-          <Box width={'100%'} justifyContent={'left'}>
-            {splitSongs.map((section) => {
-              const firstLetter = section[0].title[0];
-              return (
-                <Box key={firstLetter}>
-                  <Text mt={'1em'} color={'grey'} fontSize="lg">
-                    {firstLetter}
-                  </Text>
-                  <Divider orientation="horizontal" mb={'1em'} />
-                  {section.map((song) => {
-                    return (
-                      <Link
-                        key={song._id}
-                        to={`/song/${song._id}`}
-                      >{`${song.title}`}</Link>
-                    );
-                  })}
-                </Box>
-              );
-            })}
-          </Box>
-        </VStack>
-      </Center>
+              <Accordion allowToggle width={'100%'} maxWidth={'41em'}>
+                <AccordionItem>
+                  {({ isExpanded }) => (
+                    <>
+                      <h2>
+                        <AccordionButton>
+                          <Box flex="1" textAlign="left" color={'grey'}>
+                            Filtruj...
+                          </Box>
+                          <Tag
+                            display={
+                              areTagFiltersActive && !isExpanded
+                                ? 'inherit'
+                                : 'none'
+                            }
+                            color={'grey'}
+                          >
+                            Filtry Aktywne!
+                          </Tag>
+                          <AccordionIcon />
+                        </AccordionButton>
+                      </h2>
+                      <AccordionPanel pb={4}>
+                        <Tag
+                          colorScheme={areTagFiltersActive ? 'purple' : 'blue'}
+                          variant={areTagFiltersActive ? 'subtle' : 'solid'}
+                          m={'0.1em'}
+                          onClick={() => {
+                            if (tags.length === selectedTags.length)
+                              setSelectedTags([]);
+                            else setSelectedTags([...tags]);
+                          }}
+                        >
+                          Wszystkie
+                        </Tag>
+                        {tags.map((tag) => {
+                          const isSelected = selectedTags.includes(tag);
+                          return (
+                            <Tag
+                              colorScheme={isSelected ? 'blue' : 'purple'}
+                              variant={isSelected ? 'solid' : 'subtle'}
+                              key={tag._id}
+                              m={'0.1em'}
+                              onClick={() => {
+                                if (isSelected)
+                                  setSelectedTags([
+                                    ...selectedTags.filter(
+                                      (x) => x._id !== tag._id
+                                    ),
+                                  ]);
+                                else setSelectedTags([...selectedTags, tag]);
+                              }}
+                            >
+                              {tag.name}
+                            </Tag>
+                          );
+                        })}
+                      </AccordionPanel>
+                    </>
+                  )}
+                </AccordionItem>
+              </Accordion>
+
+              {/* <Divider orientation="horizontal" pt={'2em'} /> */}
+
+              <Box width={'100%'} justifyContent={'left'}>
+                {splitSongs.map((section) => {
+                  const firstLetter = section[0].title[0];
+                  return (
+                    <Box key={firstLetter}>
+                      <Text mt={'1em'} color={'grey'} fontSize="lg">
+                        {firstLetter}
+                      </Text>
+                      <Divider orientation="horizontal" mb={'1em'} />
+                      {section.map((song) => {
+                        return (
+                          <Link
+                            key={song._id}
+                            to={`/song/${song._id}`}
+                          >{`${song.title}`}</Link>
+                        );
+                      })}
+                    </Box>
+                  );
+                })}
+              </Box>
+            </VStack>
+          </Center>
+        </GridItem>
+        <GridItem colSpan={[0, 0, 1]}></GridItem>
+      </SimpleGrid>
     </>
   );
 }
